c2e8a35063b393d16296ac91ed647edefc13fd2d
Joris Vandeweerd <joris@statik.be>
2014-06-04 12:09:25 +0200

    added resolve URL to node, fixed bug when removing widget

--- src/ride/library/cms/content/text/UrlTextParser.php
+++ src/ride/library/cms/content/text/UrlTextParser.php
@@ -45,44 +45,7 @@ class UrlTextParser extends AbstractTextParser {
      */
     protected function replaceUrls(array $elements, $attribute) {
         foreach ($elements as $element) {
-            $url = $element->$attribute;
-            if ($url{0} == '#' || strncmp($url, 'mailto:', 7) === 0 || strncmp($url, 'http:', 5) === 0 || strncmp($url, 'https:', 6) === 0 || ($url{0} == '/' && $url{1} == '/')) {
-                continue;
-            }
-
-            if (strncmp($url, './', 2) === 0) {
-                $baseUrl = $this->node->getUrl($this->locale, $this->baseUrl);
-                $baseUrl = rtrim($baseUrl, '/');
-
-                $position = strrpos($baseUrl, '/');
-
-                $baseUrl = substr($baseUrl, 0, $position);
-                $url = substr($url, 2);
-
-                $url = $baseUrl . '/' . $url;
-            } elseif (strncmp($url, '../', 3) === 0) {
-                $baseUrl = $this->node->getUrl($this->locale, $this->baseUrl);
-                $baseUrl = rtrim($baseUrl, '/');
-
-                $position = strrpos($baseUrl, '/');
-                $baseUrl = substr($baseUrl, 0, $position);
-
-                do {
-                    $position = strrpos($baseUrl, '/');
-                    if ($position === false) {
-                        break;
-                    }
-
-                    $baseUrl = substr($baseUrl, 0, $position);
-                    $url = substr($url, 3);
-                } while (strncmp($url, '../', 3) === 0);
-
-                $url = $baseUrl . '/' . $url;
-            } else {
-                $url = $this->baseUrl . '/' . ltrim($element->$attribute, '/');
-            }
-
-            $element->$attribute = $url;
+            $element->$attribute = $this->node->resolveUrl($this->locale, $this->baseUrl, $element->$attribute);
         }
     }
 
